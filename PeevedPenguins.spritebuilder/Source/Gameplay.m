741251a0486f087a79f98d4c2ff29f78cb6124cf
Anibal Fernandez <anibal79ve@gmail.com>
2015-07-08 18:50:21 -0500

    Automatically Committed for MakeGamesWithUs

--- PeevedPenguins.spritebuilder/Source/Gameplay.m
+++ PeevedPenguins.spritebuilder/Source/Gameplay.m
@@ -139,6 +139,7 @@ static const float MIN_SPEED = 5.f;
         // follow the flying penguin
         _followPenguin = [CCActionFollow actionWithTarget:_currentPenguin worldBoundary:self.boundingBox];
         [_contentNode runAction:_followPenguin];
+        _currentPenguin.launched = TRUE;
     }
 }
 
